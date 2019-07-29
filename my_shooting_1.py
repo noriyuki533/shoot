23756470a2b2c634fb62c402c6013c8f203e4cb7
noriyuki <ka01dftn@gmail.com>
2019-07-30 07:13:09 +0900

    enemy's motion

--- my_shooting_1.py
+++ my_shooting_1.py
@@ -5,6 +5,21 @@ import random
 WINDOW_H = 200
 WINDOW_W = 200
 
+class Enemy:
+    def __init__(self):
+        self.x = 0
+        self.y = 10
+        self.vx = 2
+        self.vy = 0
+        self.size = 6
+    
+    def update(self):
+        xn = self.x + self.vx
+        if xn > WINDOW_W or xn < 0:
+            self.vx *= -1
+        
+        self.x += self.vx
+
 class Player:
     def __init__(self):
         self.x = 0
@@ -22,14 +37,19 @@ class Bullet:
         self.vx = 0
         self.vy = -3
         self.size = 3
+
     def update(self):
         self.x += self.vx
         self.y += self.vy
+
+    def setSpeed(self, speed):
+        self.vx = self.vy = speed
 
 class App:
     def __init__(self):
         pyxel.init(WINDOW_H, WINDOW_W, caption="SHOOT")
         self.player = Player()
+        self.enemy = Enemy()
         self.Bullets = []
 
         #run after all initialization
@@ -47,6 +67,7 @@ class App:
 
         
         self.player.update()
+        self.enemy.update()
         for bullet in self.Bullets:
             bullet.update()
 
@@ -57,6 +78,9 @@ class App:
 
         rs = self.player.size
         pyxel.rect(self.player.x-rs/2, self.player.y-rs/2, self.player.x+rs/2, self.player.y+rs/2, 11)
+
+        rs = self.enemy.size
+        pyxel.rect(self.enemy.x-rs/2, self.enemy.y-rs/2, self.enemy.x+rs/2, self.enemy.y+rs/2, 14)
 
         for bullet in self.Bullets:
             pyxel.circ(bullet.x, bullet.y, bullet.size, pyxel.frame_count % 16)
